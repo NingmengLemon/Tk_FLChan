3017667e0a78f184a8b850565d11b84296c91d20
NingmengLemon <60469383+NingmengLemon@users.noreply.github.com>
2025-03-10 15:23:19 +0800

    feat: additional config

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 import threading
 import tkinter as tk
 from collections.abc import Callable
-from typing import Literal, cast
+from tkinter import ttk
+from typing import Literal, TypedDict, cast
 
 from PIL import Image, ImageColor, ImageTk
 
@@ -81,6 +82,12 @@ class FLChan:
     class AlreadyActive(Exception):
         """FL Chan is active already"""
 
+    class _DragData(TypedDict):
+        x: int
+        y: int
+        dragging: bool
+        action_store: str
+
     def __init__(
         self,
         conf_path: str,
@@ -99,12 +106,14 @@ class FLChan:
             conf_path, sprites_path, color_as_transp=self._color_as_transp
         )
         self._action = [_ for _ in self._resources.keys()][0]
+        self._scale = 1.0
         self._size = [_ for _ in self._resources.values()][0][0].size
         self._drag_action = drag_action if drag_action else self._action
 
+        self._display_cfgw = False
         self._frame_idx = 0
         self._interval = 100  # ms
-        self._drag_data = {
+        self._drag_data: FLChan._DragData = {
             "x": 0,
             "y": 0,
             "dragging": False,
@@ -151,24 +160,39 @@ class FLChan:
         return list(self._resources.keys())
 
     def __load_seq(self):
-        self._sequence = [ImageTk.PhotoImage(p) for p in self._resources[self._action]]
+        size = self._resources[self._action][0].size
+        new_size = (int(size[0] * self._scale), int(size[1] * self._scale))
 
-    @__validate_active(True)
-    @__with_lock("write")
-    def set_action(self, val: str):
-        if val in self._resources.keys():
-            self._action = val
-            self.__load_seq()
-        else:
-            raise KeyError("action not found")
+        self._sequence = [
+            ImageTk.PhotoImage(p.resize(new_size, resample=Image.Resampling.BOX))
+            for p in self._resources[self._action]
+        ]
+        self._size = new_size
+        self._root.geometry("{}x{}".format(*new_size))
+
+    def set(
+        self,
+        *,
+        action: str | None = None,
+        scale: float | None = None,
+        interval: int | None = None,
+    ):
+        if interval is not None:
+            with self._rwlock.read_lock():
+                self._interval = int(interval)
+        if action is None or action not in self._resources.keys():
+            action = self._action
+        if scale is None or scale <= 0:
+            scale = self._scale
+        if (action, scale) != (self._action, self._scale):
+            with self._rwlock.write_lock():
+                self._scale = scale
+                self._action = action
+                self.__load_seq()
 
     @property
     def interval(self):
         return self._interval
-
-    @__with_lock("read")
-    def set_interval(self, val: int):
-        self._interval = int(val)
 
     @__validate_active(True)
     @__with_lock("write")
@@ -184,9 +208,16 @@ class FLChan:
         self._root = r = tk.Tk()
         self._label = ImageLabel(self._root, bg=self._color_as_transp)
         self._label.pack()
-        r.geometry("{}x{}".format(*self._size))
+        r.geometry(
+            "{}x{}+{}+{}".format(
+                *self._size,
+                self._root.winfo_screenwidth() * 4 // 5 - self._size[0],
+                self._root.winfo_screenheight() * 4 // 5 - self._size[1],
+            )
+        )
         r.wm_attributes("-topmost", True)
         r.wm_attributes("-transparentcolor", self._color_as_transp)
+        r.config(bg=self._color_as_transp)
         r.overrideredirect(True)
         r.lift()
 
@@ -202,14 +233,19 @@ class FLChan:
     def __right_click(self, event: tk.Event):
         menu = tk.Menu(self._root, tearoff=False)
         for action in self.actions:
-            menu.add_command(label=action, command=lambda a=action: self.set_action(a))
+            menu.add_command(label=action, command=lambda a=action: self.set(action=a))
         menu.add_separator()
+        menu.add_command(
+            label="Config",
+            command=self.__pop_config_window,
+            state="disabled" if self._display_cfgw else "normal",
+        )
         menu.add_command(label="Close", command=self.close)
         menu.post(event.x_root, event.y_root)
 
     @__validate_active(True)
-    def block(self):
-        self._root.wait_window(self._root)
+    def mainloop(self):
+        self._root.mainloop()
 
     @__with_lock("read")
     def _switch_frame(self):
@@ -219,18 +255,66 @@ class FLChan:
         self._label.update_pic(self._sequence[self._frame_idx])
         self._loop_task = self._root.after(self._interval, self._switch_frame)
 
+    def __pop_config_window(self):
+        self._display_cfgw = True
+        w = tk.Toplevel(self._root)
+        w.title("Config")
+        w.resizable(False, False)
+        w.transient(self._root)
+        w.wm_attributes("-toolwindow", 1)
+        w.geometry(f"+{self._root.winfo_x()}+{self._root.winfo_y()+self._size[1]}")
+
+        tk.Label(w, text="Frame interval: ").grid(row=0, column=0, sticky="e")
+        int_var = tk.IntVar(w, value=self._interval)
+        int_lab = tk.Label(w, text=f"{self._interval}ms")
+        int_lab.grid(row=0, column=1)
+        scale_i = ttk.Scale(
+            w,
+            orient=tk.HORIZONTAL,
+            from_=10,
+            to=500,
+            length=200,
+            variable=int_var,
+            command=lambda _: (
+                self.set(interval=int_var.get()),
+                int_lab.config(text=f"{int_var.get()}ms"),
+            ),
+        )
+        scale_i.grid(row=0, column=2, sticky="w")
+
+        tk.Label(w, text="Size scale: ").grid(row=1, column=0, sticky="e")
+        sca_lab = tk.Label(w, text=f"{self._scale}x")
+        sca_lab.grid(row=1, column=1)
+        sca_var = tk.DoubleVar(w, value=self._scale)
+        scale_s = ttk.Scale(
+            w,
+            orient=tk.HORIZONTAL,
+            from_=0.1,
+            to=10.0,
+            length=200,
+            variable=sca_var,
+            command=lambda _: (
+                self.set(scale=round(sca_var.get(), 1)),
+                sca_lab.config(text=f"{round(sca_var.get(), 1)}x"),
+            ),
+        )
+        scale_s.grid(row=1, column=2, sticky="w")
+
+        w.wait_window(w)
+        self._display_cfgw = False
+
     def __start_drag(self, event: tk.Event):
         self._drag_data["x"] = event.x_root
         self._drag_data["y"] = event.y_root
         self._drag_data["dragging"] = True
         if self.action != self._drag_action:
             self._drag_data["action_store"] = self.action
-            self.set_action(self._drag_action)
+            self.set(action=self._drag_action)
 
     def __stop_drag(self, _):
         self._drag_data["dragging"] = False
         if self.action != self._drag_data["action_store"]:
-            self.set_action(self._drag_data["action_store"])
+            self.set(action=self._drag_data["action_store"])
 
     def __on_drag(self, event: tk.Event):
         if self._drag_data["dragging"]:
@@ -248,7 +332,7 @@ class FLChan:
 def main():
     flchan = FLChan("artworks/Dance.txt", drag_action="Held")
     flchan.show()
-    flchan.block()
+    flchan.mainloop()
 
 
 if __name__ == "__main__":
